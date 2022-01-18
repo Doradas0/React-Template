e174406835fc640a4d88d8d3d194f0cbccd58203
Doradas <doradasm@gmail.com>
2022-01-18 17:26:41 +0000

    Adding example feacture component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Example } from "./features/Example";
+
 const App = () => {
 	return (
 		<>
 			<h1>React Template</h1>
+			<Example />
 			<p>Created by Doradas</p>
 			<p>Inspired by https://github.com/alan2207/bulletproof-react </p>
 		</>
